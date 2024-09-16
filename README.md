300f73a8a8920b4dea4d2202fe0b3a3e0564322c
Mafogaing <mafogaingrose@gmail.com>
2024-09-16 16:03:06 +0200

    page schema et template

--- README.md
+++ README.md
@@ -3,9 +3,12 @@
 Projet du cours 5BDDD (création des API et BDD)
 Utilisation de _FastAPI_
 
-Lien documentation : https://fastapi.tiangolo.com/virtual-environments/ ou https://docs.python.org/3/library/venv.html
 
 ## installation Linux
+Lien documentation
+VENV : https://fastapi.tiangolo.com/virtual-environments/ et https://docs.python.org/3/library/venv.html
+Pages statiques : https://fastapi.tiangolo.com/reference/staticfiles/
+Redirections : https://fastapi.tiangolo.com/uk/advanced/custom-response/#redirectresponse Templates : https://fastapi.tiangolo.com/advanced/templates/
 
 ```bash
 python3 -m venv venv
@@ -36,3 +39,5 @@ fastapi dev main.py
 Accès au swagger sur http://127.0.0.1:8000/docs
 accès au fichier Json http://127.0.0.1:8000/items/5?q=somequery avec (item_id=5 et q=somequery)
 Accès à la page index.html http://127.0.0.1:8000/static/index.html
+page template http://127.0.0.1:8000/items/5?q=test 
+
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from typing import Union
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+
 from dotenv import load_dotenv
 import os
+import schema
 
 load_dotenv()  # take environment variables from .env.
 
@@ -12,6 +16,8 @@ load_dotenv()  # take environment variables from .env.
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"),name="static")
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
 def read_root():
@@ -19,5 +25,8 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(request: Request, item_id: int, q: Union[str, None] = None):
+    # return {"item_id": item_id, "q": q}
+    return templates.TemplateResponse(
+        request=request, name="item.html", context={"item_id": item_id, "q": q}
+    )
